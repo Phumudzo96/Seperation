f2627de2aea3a6271e51b01ab0e4022bcca733f8
Phumudzo <103152398+Phumudzo96@users.noreply.github.com>
2023-02-15 00:08:34 +0200

    Add files via upload

---
+++ seperation.py
@@ -0,0 +1,6 @@
+sentence = input("Enter a sentence: ") # Ask the user to enter a sentence
+
+sentence1 = sentence.replace(" ", "\n") # Separate the words into new lines
+
+print(sentence1) # Result of the separated words
+
